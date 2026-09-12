729323057fdd34b04469428f0e06c6f137e32628
$wat <ruanbiasuttidesouza@gmail.com>
2026-09-12 14:22:17 -0300

    Rework e otimização dos sistemas internos do jogo + melhorias na latência de ações na batalha e sistema de nuvens na floresta

--- scripts/fight_player.gd
+++ scripts/fight_player.gd
@@ -26,3 +26,40 @@ func move_forward(x,y):
 	tween.tween_property(self, "position", Vector2(x,y), 0.5)
 	tween.tween_interval(0.2)
 	tween.tween_property(self, "position", original_position, 0.5)
+
+func taking_damage():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(0.65, 0.0, 0.0, 1.0), 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "modulate", Color(0.65, 0.0, 0.0, 1.0), 0.0)
+	tween.tween_interval(0.25)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.0)
+
+func reappearing_effect():
+	var tween = create_tween()
+	tween.tween_property(self, "visible", false, 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "visible", true, 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "visible", false, 0.0)
+	tween.tween_interval(0.25)
+	tween.tween_property(self, "visible", true, 0.0)
+
+func blocking_effect():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(0.0, 0.0, 0.65, 1.0), 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.0)
+	tween.tween_interval(0.1)
+	tween.tween_property(self, "modulate", Color(0.0, 0.0, 0.65, 1.0), 0.0)
+	tween.tween_interval(0.25)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.0)
+	await tween.finished
+
+func heal_effect():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(0.0, 0.65, 0.0, 1.0), 0.0)
+	tween.tween_interval(0.35)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.0)
